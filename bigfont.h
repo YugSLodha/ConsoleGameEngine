da1a76dcd349e522d07a971dfbe5906ae743a5a7
Yug Lodha <yuglodha81@gmail.com>
2025-02-08 00:21:05 +0530

    added font header(UNTESTED)

---
+++ bigfont.h
@@ -0,0 +1,113 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> a = {};
+
+std::vector<std::string> b = {};
+
+std::vector<std::string> c = {};
+
+std::vector<std::string> d = {};
+
+std::vector<std::string> e = { " _____ ",
+							   "|   __|",
+							   "|  |__ ",
+							   "|   __|",
+							   "|  |__ ",
+							   "|_____|" };
+
+std::vector<std::string> f = {};
+
+std::vector<std::string> g = {};
+
+std::vector<std::string> h = { "",
+							   "",
+							   "",
+							   "",
+							   "",
+							   "" };
+
+std::vector<std::string> i = { " _____ ",
+							   "|_____|",
+							   "  | |  ",
+							   "  | |  ",
+							   " _|_|_ ",
+							   "|_____|" };
+
+std::vector<std::string> j = {};
+
+std::vector<std::string> k = {};
+
+std::vector<std::string> l = {};
+
+std::vector<std::string> m = {};
+
+std::vector<std::string> n = {};
+
+std::vector<std::string> o = {};
+
+std::vector<std::string> p = {};
+
+std::vector<std::string> q = {};
+
+std::vector<std::string> r = { " _____ ",
+							   "|     \\",
+							   "|_____/",
+							   "| \\    ",
+							   "|   \\  ",
+							   "|     \\" };
+
+std::vector<std::string> s = { " _____ ",
+							   "|  ___|",
+							   "|  |__ ",
+							   "|___  |",
+							   " __|  |",
+							   "|_____|" };
+
+std::vector<std::string> t = { " _____ ",
+							   "|_____|",
+							   "  | |  ",
+							   "  | |  ",
+							   "  | |  ",
+							   "  |_|  " };
+
+std::vector<std::string> u = {};
+
+std::vector<std::string> v = {};
+
+std::vector<std::string> w = {};
+
+std::vector<std::string> x = {};
+
+std::vector<std::string> y = {};
+
+std::vector<std::string> z = {};
+
+struct FONT {
+	std::vector<std::string> A = a;
+	std::vector<std::string> B = b;
+	std::vector<std::string> C = c;
+	std::vector<std::string> D = d;
+	std::vector<std::string> E = e;
+	std::vector<std::string> F = f;
+	std::vector<std::string> G = g;
+	std::vector<std::string> H = h;
+	std::vector<std::string> I = i;
+	std::vector<std::string> J = j;
+	std::vector<std::string> K = k;
+	std::vector<std::string> L = l;
+	std::vector<std::string> M = m;
+	std::vector<std::string> N = n;
+	std::vector<std::string> O = o;
+	std::vector<std::string> P = p;
+	std::vector<std::string> Q = q;
+	std::vector<std::string> R = r;
+	std::vector<std::string> S = s;
+	std::vector<std::string> T = t;
+	std::vector<std::string> U = u;
+	std::vector<std::string> V = v;
+	std::vector<std::string> W = w;
+	std::vector<std::string> X = x;
+	std::vector<std::string> Y = y;
+	std::vector<std::string> Z = z;
+}
--- main.cpp
+++ main.cpp
@@ -1,35 +1,9 @@
-
+#include "bigfont.h"
 #include "engine.h"
 
 int main() {
-	const int FPS = 60;
-	const int width = 60;
-	const int height = 20;
+	FONT font;
+	for ()
 
-	Renderer renderer(width, height);
-	FpsManager fps(FPS);
-	Input input;
-	PhysicsEngine physicsEngine;
-	UI ui;
-
-	const std::string playText = "Press <Enter> To Play";
-	const std::string settingsText = "Press <Shift> To Open Settings";
-	const std::string exitText = "Press <Escape> To Exit";
-
-	clearScreen();
-	while (true) {
-		renderer.clearBuffer();
-		float deltaTime = fps.regulate();
-
-		renderer.drawBorder(width, height, '#', 15);
-
-		ui.addElement(playText, width / 2 - playText.length() / 2, height / 2 + 2, 1);
-		ui.addElement(settingsText, width / 2 - settingsText.length() / 2, height / 2 + 3, 1);
-		ui.addElement(exitText, width / 2 - exitText.length() / 2, height / 2 + 4, 1);
-
-		renderer.drawUI(ui);
-
-		renderer.drawBuffer();
-	}
 	return 0;
 }
