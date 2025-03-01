9b641bb34b536d2aacb0b45cf7a1b07f4038d72c
Yug Lodha <yuglodha81@gmail.com>
2025-03-01 21:59:20 +0530

    resized console

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "engine.h"
-#include <conio.h> // For _kbhit() and _getch()
+#include <conio.h>
 
 int main() {
-	int screenWidth = 40;
-	int screenHeight = 20;
+	const int screenWidth = 139;
+	const int screenHeight = 30;
+	const int FPS = 60;
 
-	// Create Camera and Renderer
 	Camera camera(0, 0);
 	Renderer renderer(screenWidth, screenHeight, &camera);
-	FPSManager fpsManager(30);
+	FPSManager fpsManager(FPS);
 
 	bool running = true;
 
@@ -17,27 +17,18 @@ int main() {
 		if (_kbhit()) {
 			char key = _getch();
 			switch (key) {
-			case 'w': camera.move(0, -1); break;
-			case 's': camera.move(0, 1); break;
 			case 'a': camera.move(-1, 0); break;
 			case 'd': camera.move(1, 0); break;
 			case 'q': running = false; break; // Quit
 			}
 		}
-
-		// 🎨 Clear & Render Scene
 		renderer.clearBuffer();
 
-		// Example: Draw a red '@' at world position (10,10)
 		renderer.drawChar(Position(10, 10), '@', Color::Red);
 		renderer.drawChar(Position(8, 2), '%', Color::Yellow);
 
 		renderer.drawBorder('#', Color::BrightWhite);
-
-		// 🖥️ Render Frame
 		renderer.drawBuffer();
-
-		// ⏳ Regulate FPS
 		fpsManager.regulate();
 	}
 
